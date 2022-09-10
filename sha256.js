85f276745708774c63752cd66babbb7d19728311
tanykazy <tanykazy@gmail.com>
2022-09-10 15:26:00 +0900

    Fix local var

--- sha256.js
+++ sha256.js
@@ -1,72 +1,69 @@
 function sha256(ascii) {
     function rightRotate(value, amount) {
         return (value >>> amount) | (value << (32 - amount));
-    };
+    }
 
-    var mathPow = Math.pow;
-    var maxWord = mathPow(2, 32);
-    var lengthProperty = 'length';
-    var i, j; // Used as a counter across the whole file
-    var result = '';
+    const maxWord = Math.pow(2, 32);
+    let result = '';
 
-    var words = [];
-    var asciiBitLength = ascii[lengthProperty] * 8;
+    let words = [];
+    const asciiBitLength = ascii.length * 8;
 
     //* caching results is optional - remove/add slash from front of this line to toggle
     // Initial hash value: first 32 bits of the fractional parts of the square roots of the first 8 primes
     // (we actually calculate the first 64, but extra values are just ignored)
-    var hash = sha256.h = sha256.h || [];
+    let hash = sha256.h = sha256.h || [];
+    // let hash = [];
     // Round constants: first 32 bits of the fractional parts of the cube roots of the first 64 primes
-    var k = sha256.k = sha256.k || [];
-    var primeCounter = k[lengthProperty];
-    /*/
-     var hash = [], k = [];
-     var primeCounter = 0;
-     //*/
+    let k = sha256.k = sha256.k || [];
+    // let k = [];
+    let primeCounter = k.length;
+    // let primeCounter = 0;
 
-    var isComposite = {};
-    for (var candidate = 2; primeCounter < 64; candidate++) {
+    let isComposite = {};
+    for (let candidate = 2; primeCounter < 64; candidate++) {
         if (!isComposite[candidate]) {
-            for (i = 0; i < 313; i += candidate) {
+            for (let i = 0; i < 313; i += candidate) {
                 isComposite[i] = candidate;
             }
-            hash[primeCounter] = (mathPow(candidate, .5) * maxWord) | 0;
-            k[primeCounter++] = (mathPow(candidate, 1 / 3) * maxWord) | 0;
+            hash[primeCounter] = (Math.pow(candidate, 0.5) * maxWord) | 0;
+            k[primeCounter++] = (Math.pow(candidate, 1 / 3) * maxWord) | 0;
         }
     }
 
     ascii += '\x80'; // Append Ƈ' bit (plus zero padding)
-    while (ascii[lengthProperty] % 64 - 56) {
+    while (ascii.length % 64 - 56) {
         ascii += '\x00'; // More zero padding
     }
 
-    for (i = 0; i < ascii[lengthProperty]; i++) {
-        j = ascii.charCodeAt(i);
+    for (let i = 0; i < ascii.length; i++) {
+        const j = ascii.charCodeAt(i);
         if (j >> 8) {
             return; // ASCII check: only accept characters in range 0-255
         }
         words[i >> 2] |= j << ((3 - i) % 4) * 8;
     }
-    words[words[lengthProperty]] = ((asciiBitLength / maxWord) | 0);
-    words[words[lengthProperty]] = (asciiBitLength);
+    words[words.length] = ((asciiBitLength / maxWord) | 0);
+    words[words.length] = (asciiBitLength);
 
     // process each chunk
-    for (j = 0; j < words[lengthProperty];) {
-        var w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
-        var oldHash = hash;
+    for (let j = 0; j < words.length;) {
+        const w = words.slice(j, j += 16); // The message is expanded into 64 words as part of the iteration
+        const oldHash = hash;
         // This is now the undefinedworking hash", often labelled as variables a...g
         // (we have to truncate as well, otherwise extra entries at the end accumulate
         hash = hash.slice(0, 8);
 
-        for (i = 0; i < 64; i++) {
-            var i2 = i + j;
+        for (let i = 0; i < 64; i++) {
             // Expand the message into 64 words
             // Used below if 
-            var w15 = w[i - 15], w2 = w[i - 2];
+            const w15 = w[i - 15];
+            const w2 = w[i - 2];
 
             // Iterate
-            var a = hash[0], e = hash[4];
-            var temp1 = hash[7]
+            const a = hash[0];
+            const e = hash[4];
+            const temp1 = hash[7]
                 + (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) // S1
                 + ((e & hash[5]) ^ ((~e) & hash[6])) // ch
                 + k[i]
@@ -79,23 +76,31 @@ function sha256(ascii) {
                 ) | 0
                 );
             // This is only used once, so *could* be moved below, but it only saves 4 bytes and makes things unreadble
-            var temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
+            const temp2 = (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) // S0
                 + ((a & hash[1]) ^ (a & hash[2]) ^ (hash[1] & hash[2])); // maj
 
             hash = [(temp1 + temp2) | 0].concat(hash); // We don't bother trimming off the extra ones, they're harmless as long as we're truncating when we do the slice()
             hash[4] = (hash[4] + temp1) | 0;
         }
 
-        for (i = 0; i < 8; i++) {
+        for (let i = 0; i < 8; i++) {
             hash[i] = (hash[i] + oldHash[i]) | 0;
         }
     }
 
-    for (i = 0; i < 8; i++) {
-        for (j = 3; j + 1; j--) {
-            var b = (hash[i] >> (j * 8)) & 255;
+    for (let i = 0; i < 8; i++) {
+        for (let j = 3; j + 1; j--) {
+            const b = (hash[i] >> (j * 8)) & 255;
             result += ((b < 16) ? 0 : '') + b.toString(16);
         }
     }
     return result;
 };
+
+
+let hash0 = sha256('abc');
+console.log(hash0 === 'ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad');
+let hash1 = sha256('ABCDEF');
+console.log(hash1);
+let hash2 = sha256('ABCDEFG');
+console.log(hash2);
